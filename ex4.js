4b875d268c6c10e5da386975c221173dd1187c87
ErebosXYZ <robertmaynou@gmail.com>
2025-03-13 20:54:05 +0100

    resolució i explicació

--- ex4.js
+++ ex4.js
@@ -4,8 +4,10 @@
 
 function rollDice() {
     // COMPLETAR
-    return;
+    return Math.floor(Math.random() * 6) + 1;
 }
+// Math.floor perquè el número sigui enter
+// Math random genera un número aleatori entre el 0 i el 1. Ho multipliquem per 6 perquè generi un número del 0 al 5.9 (5 pq serà enter pel Math.floor) i li sumo 1 perquè el resultat estigui sempre entre l'1 i el 6
 
 console.log(rollDice()); // Un número entre 1 y 6
 console.log(rollDice()); // Un número entre 1 y 6
